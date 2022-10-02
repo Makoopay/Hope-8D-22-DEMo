7de6c8eac996541958e5b09afd45e8935fef0e76
Makoopay <rectinaboy@gmail.com>
2022-10-02 08:42:54 +0700

    Demo Game.LOG(1)

---
+++ Demo_Game/Assets/Scripts/CursorVisibility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorVisibility : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnlockMouse();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
---
+++ Demo_Game/Assets/Scripts/DeleteText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteText : MonoBehaviour
+{
+    public GameObject Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the Enemy tag
+        if (collision.gameObject.tag == "Player")
+        {
+            //tells code that you died
+            Destroy(Text);
+
+
+        }
+    }
+}
